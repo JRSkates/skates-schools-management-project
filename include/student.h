3f96eb765ba85c81da026f39b1c2c35bdfabe5a5
JRSkates <skatesproduction@gmail.com>
2025-01-22 09:58:26 +0000

    student and teacher header files created

---
+++ include/student.h
@@ -0,0 +1,20 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+typedef struct StudentNode { 
+    int id; 
+    char name[50]; 
+    char subjects[10][50]; // Fixed-size array for subject names 
+    int num_subjects; // Number of subjects the student is enrolled in
+    StudentNode *next; // Next student 
+} StudentNode;
+
+
+void add_student(StudentNode **head, int id, const char *name); 
+void add_subject_to_student(StudentNode *student, const char *subject_name); 
+void print_students(StudentNode *head);
+void student_by_subject(StudentNode *head, char *subject_name);
+
+
+
+#endif // _STUDENT_H_
---
+++ include/teacher.h
@@ -0,0 +1,17 @@
+#ifndef _TEACHER_H_
+#define _TEACHER_H_
+
+typedef struct TeacherNode { 
+    int id; 
+    char name[50]; 
+    char subject[50]; // The subject they teach struct 
+    TeacherNode *next; // Next teacher 
+} TeacherNode;
+
+void add_teacher(TeacherNode **head, int id, const char *name, const char *subject); 
+void print_teachers(TeacherNode *head);
+void print_teacher_by_subject(TeacherNode *head, char *subject);
+
+
+
+#endif // _TEACHER_H_
